333f0d28a53127179dfe70d15d3221cf77eb9d66
Edem Shukuriev <2nevseti6@mail.ru>
2025-10-10 14:40:36 +0300

    Добавил тесты

---
+++ test.bat
@@ -0,0 +1,4 @@
+python shell5.py --vfs-path vfs-xml/stage5.xml --script test/test-stage5.txt
+python shell5.py --vfs-path vfs-xml/minimal.xml --script test/test-min.txt
+python shell5.py --vfs-path vfs-xml/deep.xml --script test/test-deep.txt
+pauseq
